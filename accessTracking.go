3e920dd52b64463a892e26cae5f6b3e10d6f083b
milbertk <milbert.retana@kuantumk.com>
2025-08-25 23:16:18 -0600

    Start repository first commit

---
+++ accessTracking.go
@@ -0,0 +1,43 @@
+package accesstracking
+
+import (
+	"fmt"
+
+	"github.com/milbertk/databasesmng"
+)
+
+// Insert inserts the FirebaseUser into the usfirebasedata table
+func (lt *LoginTracking) Insert() error {
+	db, err := databasesmng.CreateConnection()
+	if err != nil {
+		return fmt.Errorf("❌ DB connection error: %v", err)
+	}
+
+	query := `
+		INSERT INTO public.logintracking (
+	userid, email, datelocalacces, ip, platform,
+	macaddress, browser, countrycode, gmttime, lang
+)
+VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10);
+	`
+
+	_, err = db.Exec(query,
+		lt.UserID,
+		lt.Email,
+		lt.DateLocalAcces,
+		lt.IP,
+		lt.Platform,
+		lt.MacAddress,
+		lt.Browser,
+		lt.CountryCode,
+		lt.GMTTime,
+		lt.Lang,
+	)
+
+	if err != nil {
+		return fmt.Errorf("❌ Failed to insert user: %v", err)
+	}
+
+	fmt.Println("✅ Tracking inserted succesfully")
+	return nil
+}
---
+++ tracking.go
@@ -0,0 +1,14 @@
+package accesstracking
+
+type LoginTracking struct {
+	UserID         string `db:"userid"`
+	Email          string `db:"email"`
+	DateLocalAcces string `db:"datelocalacces"`
+	IP             string `db:"ip"`
+	Platform       string `db:"platform"`
+	MacAddress     string `db:"macaddress"`
+	Browser        string `db:"browser"`
+	CountryCode    string `db:"countrycode"`
+	GMTTime        string `db:"gmttime"`
+	Lang           string `db:"lang"`
+}
